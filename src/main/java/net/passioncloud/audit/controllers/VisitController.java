134526e805fb6b68bc03e1497667e435c34917a8
SSG <gssenyonjo@gmail.com>
2023-02-14 07:45:46 +0300

    Implemented adding a new visit

--- src/main/java/net/passioncloud/audit/controllers/VisitController.java
+++ src/main/java/net/passioncloud/audit/controllers/VisitController.java
@@ -7,7 +7,10 @@ import net.passioncloud.audit.models.Visit;
 import net.passioncloud.audit.repositories.VisitRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,6 +27,18 @@ public class VisitController {
         log.info("Getting visits");
         Iterable<Visit> visits = visitRepository.findAll();
         model.addAttribute("visits", visits);
+        return "visits/index";
+    }
+
+    @GetMapping("/add")
+    public String addVisit(Model model) {
+        model.addAttribute("visit", new Visit());
+        return "visits/add-visit";
+    }
+
+    @PostMapping("/add")
+    public String addVisit(@ModelAttribute Visit visit, BindingResult errors, Model model) {
+        visitRepository.save(visit);
         return "visits/index";
     }
 
