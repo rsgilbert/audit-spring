028f2ec6100064ef41b7fb136bada3b5955f7a7f
SSG <gssenyonjo@gmail.com>
2023-02-14 07:21:52 +0300

    Successfully setup visits/index.html and index.html starter pages

--- src/main/java/net/passioncloud/audit/controllers/IndexController.java
+++ src/main/java/net/passioncloud/audit/controllers/IndexController.java
@@ -1,16 +1,19 @@
 package net.passioncloud.audit.controllers;
 
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Log4j2
 @Controller
 @RequestMapping("")
 public class IndexController {
 
     @GetMapping({ "", "/" })
     public String index(){
+        log.info("Index page");
         return "index";
     }
 }
